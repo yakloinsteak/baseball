b9f77b950f3d02c2621bff56595a0c08f954a619
Todd Blackman <todd@gigapan.com>
2013-09-12 20:32:09 -0700

    Stats helper for sorting refactored

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,21 +1,23 @@
 module StatsHelper
-  #TDB
-  def stats_link_to name, order_by
+  def sortable_link_to name, order_by
     extra = ''
+
+    # Toggle sort direction if order param was for this column
     if order_by == params[:order_by]
       new_order_by = order_by.sub(/desc/, 'asc')
     else
       new_order_by = order_by
     end
 
-    if order_by.sub(/desc|asc/, '') == params[:order_by].sub(/desc|asc/, '')
-      if new_order_by =~ /asc/
-        extra = "&darr;".html_safe
-      else
-        extra = "&uarr;".html_safe
-      end
+    # Annotate with arrows if we've sorted this column
+    if _sans_direction(order_by) == _sans_direction(params[:order_by])
+      extra = new_order_by =~ /asc/ ?  "&darr;" : "&uarr;"
     end
 
-    link_to(name.html_safe+extra, stats_path(format: :html, order_by: new_order_by), remote: true)
+    link_to (extra+name).html_safe, stats_path(format: :html, order_by: new_order_by), remote: true
+  end
+
+  def _sans_direction str
+    str.try(:sub, /desc|asc/, '')
   end
 end
--- spec/helpers/stats_helper_spec.rb
+++ spec/helpers/stats_helper_spec.rb
@@ -1,14 +1,28 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the StatsHelper. For example:
-#
-# describe StatsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe StatsHelper do
+  def set_order_by_param value; helper.params[:order_by] = value; end
+
+  context "sortable_link_to" do
+    let(:link_html) { helper.sortable_link_to 'Runs', 'runs desc' }
+
+    it "should create a remote link" do
+      link_html.should == '<a data-remote="true" href="/stats.html?order_by=runs+desc">Runs</a>'
+    end
+
+    it "should show opposite sort order if sorted ascending" do
+      set_order_by_param('runs asc')
+      link_html.should == '<a data-remote="true" href="/stats.html?order_by=runs+desc">&uarr;Runs</a>'
+    end
+
+    it "should show opposite sort order if sorted descending" do
+      set_order_by_param('runs desc')
+      link_html.should == '<a data-remote="true" href="/stats.html?order_by=runs+asc">&darr;Runs</a>'
+    end
+
+    it "should show sorting in descending order if param sort order is another column" do
+      set_order_by_param('something_else asc')
+      link_html.should == '<a data-remote="true" href="/stats.html?order_by=runs+desc">Runs</a>'
+    end
+  end
 end
